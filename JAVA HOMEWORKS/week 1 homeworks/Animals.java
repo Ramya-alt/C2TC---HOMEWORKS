d19adc31804fba1f3413796c2b3f66013a884150
Ramya.K <112683773+Ramya-alt@users.noreply.github.com>
2022-11-20 22:16:20 +0530

    Add files via upload
    PROGRAM - Creating a class called animal, create objects, attributes, methods for the class

---
+++ JAVA HOMEWORKS/week 1 homeworks/Animals.java
@@ -0,0 +1,26 @@
+package c2tc;
+
+public class Animals {
+	string name;
+	string color;
+	string sound;
+}
+piblic void typeofanimal()
+{
+	System.out.println('The type pf animal is: carnivorous')
+}
+public void food()
+{
+	System.out.println('Animal is eating food ')
+}
+	
+
+	public static void main(String[] args) {
+		Animal.tiger=new Animal;
+		Animal.lion=new Animal;
+		tiger.typeofanimal()
+		lion.food()
+	
+	}
+
+}
